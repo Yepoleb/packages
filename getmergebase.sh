df519f58cf49b1b8f32633d7eb1e4a89237a3655
Gabriel Huber <mail@gabrielhuber.at>
2023-03-08 17:47:58 +0100

    Improve debug information of germergebase.sh

--- getmergebase.sh
+++ getmergebase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# CI helper script to checkout the current packages.json and
+# the version at the merge base to compare to.
+
 set -e
-set -x
 
 targetRepository="https://github.com/$GITHUB_REPOSITORY"
 targetBranch="$GITHUB_BASE_REF"
@@ -10,8 +12,8 @@ git branch merge-branch
 cp packages.json packages.json.bak
 git fetch "$targetRepository" "$targetBranch:base"
 # Get packages.json at the branching point
-git status
-git merge-base merge-branch base
-git checkout "$(git merge-base merge-branch base)" packages.json
+mergeBase="$(git merge-base merge-branch base)"
+echo "Comparing against packages.json at $mergeBase"
+git checkout "$mergeBase" packages.json
 mv packages.json packages_old.json
 mv packages.json.bak packages.json
